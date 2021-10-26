0db70eaaee7a53e6043e01eda605c2ecdf12ed00
Tamara Oliveira <tamaravaoliveira@gmail.com>
2021-10-26 17:57:06 +0000

    Preparando o CRUD

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -2,10 +2,13 @@ package application.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import application.models.Livro;
 import application.repositories.LivroRepository;
-
 import org.springframework.ui.Model;
+
 
 @Controller
 @RequestMapping("/livro")
@@ -15,10 +18,23 @@ public class LivroController {
     @RequestMapping("/list")
     public String list(Model model) {
         model.addAttribute("livros", livrosRepo.findAll());
-        return "list.jsp"; 
+        return "list.jsp";
     }
   public String listar(){
     return "livro/list.jsp";
   }
+  @RequestMapping("/insert")
+  public String formInsert(){
+    return "insert.jsp";
 
+  }
+  @RequestMapping(value="/insert",method=RequestMethod.POST)
+  public String saveInsert(@RequestParam("titulo")String titulo){
+    
+    Livro livro = new Livro();
+    livro.setTitulo(titulo);
+    livrosRepo.save(livro);
+    return "redirect:/livro/list";
+
+  }
 }
