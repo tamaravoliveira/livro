f068f550ddfc28b09cbecdc55ed3e3bee8b29fa8
Tamara Oliveira <tamaravaoliveira@gmail.com>
2021-11-22 20:08:00 +0000

    Comentando

--- src/main/java/application/controllers/HomeController.java
+++ src/main/java/application/controllers/HomeController.java
@@ -1,7 +1,7 @@
-package application.controllers;
+package application.controllers; //pacote da hierarquização de pastas.
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller; //import controlador de tipo de anotação, normalmente usado em combinação com métodos de manipulador anotados com base na RequestMapping. 
+import org.springframework.web.bind.annotation.RequestMapping; //import
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
