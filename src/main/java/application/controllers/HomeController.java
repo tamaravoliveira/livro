dcda861ef351b7bfd241b1be5e17196e4568e45d
Tamara Oliveira <tamaravaoliveira@gmail.com>
2021-11-22 20:33:46 +0000

    Comentando...

--- src/main/java/application/controllers/HomeController.java
+++ src/main/java/application/controllers/HomeController.java
@@ -1,13 +1,13 @@
 package application.controllers; //pacote da hierarquização de pastas.
 
 import org.springframework.stereotype.Controller; //import controlador de tipo de anotação, normalmente usado em combinação com métodos de manipulador anotados com base na RequestMapping. 
-import org.springframework.web.bind.annotation.RequestMapping; //import
+import org.springframework.web.bind.annotation.RequestMapping; //import do RequestMapping, é usado para mapear solicitações da web em métodos manipuladores em classes de manipulação de solicitações. 
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller //inicializando o Controller.
 public class HomeController {
     @RequestMapping(method=RequestMethod.GET)
     public String index() {
-        return "home/index.jsp";
+        return "home/index.jsp"; //retorna o arquivo index.jsp que se encontra na pasta home.
     }
 }
