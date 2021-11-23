8468a78f8ad04fbd9efe3f0f1715d99a7eca05d6
Tamara Oliveira <tamaravaoliveira@gmail.com>
2021-11-23 00:55:22 +0000

    Comentando...

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -3,7 +3,7 @@ package application; //pacote que vem do java.
 import org.springframework.boot.SpringApplication; //importando o SpringApplication.
 import org.springframework.boot.autoconfigure.SpringBootApplication; //import que executa o programa.
 
-@SpringBootApplication
+@SpringBootApplication //anotação SpringBootApplication.
 public class Application { //criando classe Application.
     public static void main(String[] args) { //criando método main.
         SpringApplication.run(Application.class, args); //classe que executa o programa.
