3353f12c267c046165ce54a7ac3078a9eb342c31
Tamara Oliveira <tamaravaoliveira@gmail.com>
2021-11-23 19:02:18 +0000

    comentários...

--- src/main/java/application/models/Livro.java
+++ src/main/java/application/models/Livro.java
@@ -1,32 +1,32 @@
-package application.models;
+package application.models; //pacote para manipulação de informações.
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.Entity; //import do Entity do Java.
+import javax.persistence.GeneratedValue; //import do GeneratedValue do Java.
+import javax.persistence.GenerationType; //import do GenerationType do Java.
+import javax.persistence.Id; //import do Id do Java.
+import javax.persistence.Table; //import do Table do Java.
 
-@Entity
-@Table(name="livros")
-public class Livro {
+@Entity //anotação para indicar que a classe livro é uma entidade.
+@Table(name="livros") //anotação que especifíca a tabela para o Entity.
+public class Livro { //criando classe Livro.
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String titulo;
+    @Id //anotação que especifíca a chave primário da Entity.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //informando a geração do valor do identificador único.
+    private int id; //criando variável id.
+    private String titulo; //criando variável titulo.
 
-    public int getId() {
-        return id;
+    public int getId() { //criando função getId(), tem como funcionalidade pegar o valor de id.
+        return id; //retorna o valor de id.
     }
-    public void setId(int id) {
-        this.id = id;
+    public void setId(int id) { //criando função setId(), tem como funcionalidade definir o valor de id.
+        this.id = id; //configura o valor de id, conforme o passado no parâmetro.
     }
-    public String getTitulo() {
-        return titulo;
+    public String getTitulo() { //criando função getTitulo(), tem como funcionalidade pegar o valor de titulo.
+        return titulo; ////retorna o valor de titulo.
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setTitulo(String titulo) { //criando função setTitulo(), tem como funcionalidade definir o valor de titulo.
+        this.titulo = titulo; ////configura o valor de titulo, conforme o passado no parâmetro.
     }
 
 
--- src/main/java/application/repositories/LivroRepository.java
+++ src/main/java/application/repositories/LivroRepository.java
@@ -1,7 +1,7 @@
-package application.repositories;
-import org.springframework.data.repository.CrudRepository;
+package application.repositories; //pacote de repositórios.
+import org.springframework.data.repository.CrudRepository; //import do CrudRepository do springframework.
 
-import application.models.Livro;
-public interface LivroRepository extends CrudRepository<Livro, Integer> {
+import application.models.Livro; //import do model livro da aplicação.
+public interface LivroRepository extends CrudRepository<Livro, Integer> { //criando  interface livroRepository, com a extensão do repositório crud.
     
 }
